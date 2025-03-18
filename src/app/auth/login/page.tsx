7538f5dcb7f1d16342ebd1fbcfd0dfe3e218f5d7
ImSidow <sidow.code@gmail.com>
2025-03-18 20:19:05 +0300

    added: login page ui

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,8 +1,8 @@
-import { Logo } from '@/components/shadcn/logo';
-import { Label } from '@/components/shadcn/ui/label';
-import { Button } from '@/components/shadcn/ui/button';
-import { Input } from '@/components/shadcn/ui/input';
-import Link from 'next/link';
+import { Logo } from "@/components/shadcn/logo";
+import { Label } from "@/components/shadcn/ui/label";
+import { Button } from "@/components/shadcn/ui/button";
+import { Input } from "@/components/shadcn/ui/input";
+import Link from "next/link";
 
 export default function LoginPage() {
     return (
@@ -13,19 +13,11 @@ export default function LoginPage() {
             >
                 <div className="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
                     <div className="text-center">
-                        <Link
-                            href="/"
-                            aria-label="go home"
-                            className="mx-auto block w-fit"
-                        >
+                        <Link href="/" aria-label="go home" className="mx-auto block w-fit">
                             <Logo />
                         </Link>
-                        <h1 className="mb-1 mt-4 text-xl font-semibold">
-                            Sign In to Tailus UI
-                        </h1>
-                        <p className="text-sm">
-                            Welcome back! Sign in to continue
-                        </p>
+                        <h1 className="mb-1 mt-4 text-xl font-semibold">Sign In to e-invoice</h1>
+                        <p className="text-sm">Welcome back! Sign in to continue</p>
                     </div>
 
                     <div className="mt-6 space-y-6">
@@ -33,38 +25,21 @@ export default function LoginPage() {
                             <Label htmlFor="email" className="block text-sm">
                                 Username
                             </Label>
-                            <Input
-                                type="email"
-                                required
-                                name="email"
-                                id="email"
-                            />
+                            <Input type="email" required name="email" id="email" placeholder="Enter email" />
                         </div>
 
                         <div className="space-y-0.5">
                             <div className="flex items-center justify-between">
-                                <Label
-                                    htmlFor="pwd"
-                                    className="text-title text-sm"
-                                >
+                                <Label htmlFor="pwd" className="text-title text-sm">
                                     Password
                                 </Label>
                                 <Button asChild variant="link" size="sm">
-                                    <Link
-                                        href="#"
-                                        className="link intent-info variant-ghost text-sm"
-                                    >
+                                    <Link href="#" className="link intent-info variant-ghost text-sm">
                                         Forgot your Password ?
                                     </Link>
                                 </Button>
                             </div>
-                            <Input
-                                type="password"
-                                required
-                                name="pwd"
-                                id="pwd"
-                                className="input sz-md variant-mixed"
-                            />
+                            <Input type="password" required name="pwd" id="pwd" className="input sz-md variant-mixed" placeholder="Enter password" />
                         </div>
 
                         <Button className="w-full">Sign In</Button>
@@ -72,20 +47,13 @@ export default function LoginPage() {
 
                     <div className="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
                         <hr className="border-dashed" />
-                        <span className="text-muted-foreground text-xs">
-                            Or continue With
-                        </span>
+                        <span className="text-muted-foreground text-xs">Or continue With</span>
                         <hr className="border-dashed" />
                     </div>
 
-                    <div className="grid grid-cols-2 gap-3">
+                    <div className="grid grid-cols-1 gap-3">
                         <Button type="button" variant="outline">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="0.98em"
-                                height="1em"
-                                viewBox="0 0 256 262"
-                            >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="0.98em" height="1em" viewBox="0 0 256 262">
                                 <path
                                     fill="#4285f4"
                                     d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622l38.755 30.023l2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
@@ -104,32 +72,6 @@ export default function LoginPage() {
                                 ></path>
                             </svg>
                             <span>Google</span>
-                        </Button>
-                        <Button type="button" variant="outline">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="1em"
-                                height="1em"
-                                viewBox="0 0 256 256"
-                            >
-                                <path
-                                    fill="#f1511b"
-                                    d="M121.666 121.666H0V0h121.666z"
-                                ></path>
-                                <path
-                                    fill="#80cc28"
-                                    d="M256 121.666H134.335V0H256z"
-                                ></path>
-                                <path
-                                    fill="#00adef"
-                                    d="M121.663 256.002H0V134.336h121.663z"
-                                ></path>
-                                <path
-                                    fill="#fbbc09"
-                                    d="M256 256.002H134.335V134.336H256z"
-                                ></path>
-                            </svg>
-                            <span>Microsoft</span>
                         </Button>
                     </div>
                 </div>
